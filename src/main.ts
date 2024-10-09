a5f0f38d4b62b86acd6c90cda0898eb8d10b363d
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-10-09 17:11:02 -0300

    add compilation check script

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address("Rua josé Cícero cardía", 47, "18120000", "mairin
 customer.address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "item 1", 10);
-const item2 = new OrderItem("1", "item 2", 15);
+const item1 = new OrderItem("1", "item 1", 10, "product 1", 4);
+const item2 = new OrderItem("1", "item 2", 15, "product 2", 5);
 
 const order = new Order("1", "123", [item1, item2]);
