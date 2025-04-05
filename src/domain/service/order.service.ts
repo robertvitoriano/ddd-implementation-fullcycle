3accdb69ee239d5073970e911a6b98c690ee5fdb
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-04-04 22:33:29 -0300

    add repository method calls to servics

--- src/domain/service/order.service.ts
+++ src/domain/service/order.service.ts
@@ -1,8 +1,10 @@
+import { CustomerRepository } from "../../infrastructure/customer/repository/sequelize/customer.repository"
 import Customer from "../entity/customer"
 import { Order } from "../entity/order"
 import { OrderItem } from "../entity/order_item"
 import { v4 as uuid } from "uuid"
 export class OrderService {
+  constructor() {}
   static total(orders: Order[]): number {
     return orders.reduce((acc: number, currentValue: Order) => {
       return acc + currentValue.total()
@@ -12,6 +14,9 @@ export class OrderService {
   static placeOrder(customer: Customer, items: OrderItem[]): Order {
     const newOrder = new Order(uuid(), customer.id, items)
     customer.addRewardPoints(newOrder.total() / 2)
+    const customerRepository = new CustomerRepository()
+    customerRepository.update(customer)
+
     return newOrder
   }
 }
--- src/domain/service/product.service.ts
+++ src/domain/service/product.service.ts
@@ -1,9 +1,13 @@
+import { ProductRepository } from "../../infrastructure/product/repository/sequelize/product.repository"
 import { Product } from "../entity/product"
 
 export class ProductsService {
-  static increasePrice(products: Product[], percentage: number): void {
-    products.forEach((product: Product) => {
+  static async increasePrice(products: Product[], percentage: number): Promise<void> {
+    const productsRepository = new ProductRepository()
+
+    for (const product of products) {
       product.changePrice((product.price * percentage) / 100 + product.price)
-    })
+      await productsRepository.update(product)
+    }
   }
 }
